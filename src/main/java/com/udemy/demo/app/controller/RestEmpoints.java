6a26973f52b998afdb8dfbeb8dc5780b1ea18ea5
cleefsouza <a.cleef.souza@gmail.com>
2019-03-24 02:53:33 -0300

    properties

--- src/main/java/com/udemy/demo/app/controller/RestEmpoints.java
+++ src/main/java/com/udemy/demo/app/controller/RestEmpoints.java
@@ -1,5 +1,6 @@
 package com.udemy.demo.app.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,19 @@ import com.udemy.demo.app.model.Course;
 
 @RestController
 public class RestEmpoints {
+
+	@Value("${default.course.name}")
+	private String cName;
+
+	@Value("${default.course.chapterCount}")
+	private int cChapterCount;
+	
+	@RequestMapping("/defaultCourse")
+	public Course getDefaultCourse(
+			@RequestParam(value = "name", defaultValue = "Spring Boot", required = false) String name,
+			@RequestParam(value = "chapterCount", defaultValue = "2", required = false) int chapterCount) {
+		return new Course(cName, cChapterCount);
+	}
 
 	@RequestMapping("/course")
 	public Course getEmpoint(@RequestParam(value = "name", defaultValue = "Spring Boot", required = false) String name,
